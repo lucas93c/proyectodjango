c72af48d7245df27c564c0c285d917810e026d5f
lucas93c <lucascabralpr@gmail.com>
2024-07-13 19:11:41 -0300

    URLs

--- src/django/form/admin.py
+++ src/django/form/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from form.models import Persona
 # Register your models here.
+
+admin.site.register(Persona)
--- src/django/form/models.py
+++ src/django/form/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, MaxLengthValidator, RegexValidator
 
 # Create your models here.
+
+class Persona(models.Model):
+    full_name = models.CharField(max_length=30,null=False)
+    edad = models.IntegerField(null=False)
+    cuil = models.CharField(
+        primary_key=True,
+        null=False,
+        max_length=11, 
+        validators=[
+            MinLengthValidator(8), 
+            RegexValidator(r'^\d{8,11}$', message="El CUIL debe tener entre 8 y 11 dígitos.")
+        ]
+    )
+    domicilio = models.CharField(max_length=50)
+    email = models.EmailField(null=False)
+    telefono = models.CharField(
+        null=False,
+        max_length=12,
+        validators=[
+            MinLengthValidator(8), 
+            RegexValidator(r'^\d{8,12}$', message="El teléfono debe tener entre 8 y 12 dígitos.")
+        ]
+    )
+   
