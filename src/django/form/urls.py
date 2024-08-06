088ed8cd26bf16bdae168778771c57f9e68d6dec
lucas93c <lucascabralpr@gmail.com>
2024-08-05 21:34:20 -0300

    hide okmsj

--- src/django/form/urls.py
+++ src/django/form/urls.py
@@ -6,8 +6,6 @@ from .views import formulario
 app_name = 'form' ## namespace
 
 urlpatterns = [
-    path('hola/', views.hola),
-    path('persona/<int:cuil>', views.persona),
     path('', views.home, name='home'),    
     path ('formulario/', formulario),
 ]
